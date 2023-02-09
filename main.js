805f3da96d63d8c2c1ddb1bb0d934a673ed8ac63
Gilad <giladmp@gmail.com>
2023-02-09 11:37:57 +0100

    add basic sepertation of concerns

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
-const plane = document.querySelector('.plane')
+
 let width = 9 // 6 seats, 2 windows & 1 path per row 
 let rows = 30
-let isCouple = false // true if we choose to joint seats, false for one passenger
-let firstSeat = ''
-let secondSeat = ''
-let oneSeat = ''
-
-
 // const squares = []
 
 function createPlane() {
+    const plane = document.querySelector('.plane')
     for (let i = 0; i < width * rows; i++) {
         const square = document.createElement('div')
         square.setAttribute('id', i)
@@ -28,61 +23,19 @@ function createPlane() {
             seatShape.setAttribute('class', 'seat-shape')
             square.appendChild(seatShape)
             seatShape.innerHTML = square.getAttribute('name').toUpperCase()
-            seatShape.addEventListener('click', chooseSeats)
+            seatShape.addEventListener('click', onChooseSeat)
         }
     }
 }
 
-function getSeatLetter(squareNum) {
-    switch (squareNum % width) {
-        case 1:
-            return 'a'
-        case 2:
-            return 'b'
-        case 3:
-            return 'c'
-        case 5:
-            return 'd'
-        case 6:
-            return 'e'
-        case 7:
-            return 'f'
-    }
+function onChooseSeat(event) {
+    chooseSeats(event)
 }
 
-function chooseSeats(event) {
-    const seat = event.target
-    if (isCouple) {   
-        if (secondSeat === seat && firstSeat !== '') {
-            secondSeat = ''
-            seat.style.backgroundColor = ''
-        } else if (firstSeat === seat && secondSeat === '') {
-            firstSeat = ''
-            seat.style.backgroundColor = ''
-        } else if (firstSeat === seat && secondSeat !== '') {
-            firstSeat = secondSeat
-            secondSeat = ''
-            seat.style.backgroundColor = ''
-        } else if (firstSeat !== '' && secondSeat === '') {
-            secondSeat = seat
-            seat.style.backgroundColor = 'green'
-        } else if (firstSeat === '' && secondSeat === '') {
-            firstSeat = seat
-            seat.style.backgroundColor = 'green'
-        }
+function paintSeat(seat, paint) {
+    if (paint) {
+        seat.style.backgroundColor = '#c3195d'
     } else {
-        if (oneSeat === seat) {
-            oneSeat = ''
-            seat.style.backgroundColor = ''
-        } else if (oneSeat !== '') {
-            oneSeat.style.backgroundColor = ''
-            oneSeat = seat
-            seat.style.backgroundColor = 'red'
-        } else if (oneSeat === '') {
-            oneSeat = seat
-            seat.style.backgroundColor = 'red'
-        }
+        seat.style.backgroundColor = ''
     }
 }
-
-createPlane()
---
+++ service.js
@@ -0,0 +1,56 @@
+let isCouple = true // true if we choose to joint seats, false for one passenger
+let firstSeat = ''
+let secondSeat = ''
+let oneSeat = ''
+
+function getSeatLetter(squareNum) {
+    switch (squareNum % 9) {
+        case 1:
+            return 'a'
+        case 2:
+            return 'b'
+        case 3:
+            return 'c'
+        case 5:
+            return 'd'
+        case 6:
+            return 'e'
+        case 7:
+            return 'f'
+    }
+}
+
+function chooseSeats(event) {
+    const seat = event.target
+    if (isCouple) {   
+        if (secondSeat === seat && firstSeat !== '') {
+            secondSeat = ''
+            paintSeat(seat, false)
+        } else if (firstSeat === seat && secondSeat === '') {
+            firstSeat = ''
+            paintSeat(seat, false)
+        } else if (firstSeat === seat && secondSeat !== '') {
+            firstSeat = secondSeat
+            secondSeat = ''
+            paintSeat(seat, false)
+        } else if (firstSeat !== '' && secondSeat === '') {
+            secondSeat = seat
+            paintSeat(seat, true)
+        } else if (firstSeat === '' && secondSeat === '') {
+            firstSeat = seat
+            paintSeat(seat, true)
+        }
+    } else {
+        if (oneSeat === seat) {
+            oneSeat = ''
+            paintSeat(seat, false)
+        } else if (oneSeat !== '') {
+            oneSeat.style.backgroundColor = ''
+            oneSeat = seat
+            paintSeat(seat, true)
+        } else if (oneSeat === '') {
+            oneSeat = seat
+            paintSeat(seat, true)
+        }
+    }
+}
